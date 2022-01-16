9ff588cebce11f2dac99be3c4b66ee49a60f78ac
davidwillianx <davidcbsi@gmail.com>
2022-01-16 18:15:53 -0300

    skip generated test

--- src/test/java/com/example/pismobank/PismobankApplicationTests.java
+++ src/test/java/com/example/pismobank/PismobankApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.pismobank;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PismobankApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
