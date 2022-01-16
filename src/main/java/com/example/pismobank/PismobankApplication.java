5c1dba2cfd212e8179849c76b3d88c4dd2a927ae
davidwillianx <davidcbsi@gmail.com>
2022-01-16 11:20:01 -0300

    project startup

---
+++ src/main/java/com/example/pismobank/PismobankApplication.java
@@ -0,0 +1,13 @@
+package com.example.pismobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismobankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PismobankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pismobank/PismobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pismobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismobankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
