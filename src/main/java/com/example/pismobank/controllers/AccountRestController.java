f0e32589914cc94976626065ae359db2b0646bb2
davidwillianx <davidcbsi@gmail.com>
2022-01-16 16:11:49 -0300

    Endpoint name convention

--- src/main/java/com/example/pismobank/controllers/AccountRestController.java
+++ src/main/java/com/example/pismobank/controllers/AccountRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("accounts")
 @RestController
 @RequiredArgsConstructor
-public class AccountController {
+public class AccountRestController {
     private final AccountService service;
 
     @PostMapping
