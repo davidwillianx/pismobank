0d24ebafc8b20eb38313ca61cf0b27cbaf5c8647
davidwillianx <davidcbsi@gmail.com>
2022-01-17 09:21:24 -0300

    Removing unnecessary mapping features

--- src/main/java/com/example/pismobank/models/OperationType.java
+++ src/main/java/com/example/pismobank/models/OperationType.java
@@ -1,17 +1,20 @@
 package com.example.pismobank.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 @Entity
 @Table
 public class OperationType {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String description;
 }
