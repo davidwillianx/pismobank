ac6840990fdb8b9bf608185d22a3a3ea50dabd60
davidwillianx <davidcbsi@gmail.com>
2022-01-17 09:18:59 -0300

    adding mapping labels to fit the docs

--- src/main/java/com/example/pismobank/models/dtos/TransactionDTO.java
+++ src/main/java/com/example/pismobank/models/dtos/TransactionDTO.java
@@ -1,5 +1,6 @@
 package com.example.pismobank.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,9 @@ import java.math.BigDecimal;
 @Setter
 @Getter
 public class TransactionDTO {
+    @JsonProperty("account_id")
     private Long accountId;
+    @JsonProperty("operation_type_id")
     private Long operationTypeId;
     private BigDecimal amount;
 }
