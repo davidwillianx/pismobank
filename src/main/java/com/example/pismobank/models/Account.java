f71a8073b1f37fb31ae0466f8b3f979638b8fe4d
davidwillianx <davidcbsi@gmail.com>
2022-01-19 19:07:07 -0300

    Account new attribute

--- src/main/java/com/example/pismobank/models/Account.java
+++ src/main/java/com/example/pismobank/models/Account.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @EqualsAndHashCode
 @Setter
@@ -16,4 +17,6 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String documentNumber;
+    @Column(name = "limite")
+    private BigDecimal limit;
 }
